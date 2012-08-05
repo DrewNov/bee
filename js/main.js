119c337c23b510cb4bb722df46d8fcbaf23601a8
DrewNov <andrey7777777@yandex.ru>
2012-08-05 14:33:08 +0300

    First successful commit! :D

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ window.onload = function (){
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     console.log('hello word');
+    //comment for testing of Git
 }
