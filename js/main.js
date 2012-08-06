77f30e335ac90f71524077974aa5b4f5b955737a
Voltash <Volt.zt@gmail.com>
2012-08-06 15:35:08 +0300

    fix bug with px

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@ $(document).bind("mobileinit", function(){
 $(document).ready(function(){
 
     var gameDOM  = $('.game');
-    gameDOM.css({'height': window.screen.availHeight-15+'px', 'width': window.height});
+    gameDOM.css({'height': window.screen.availHeight-15+'px', 'width': window.height+'px'});
 
     var metaTag = $('meta[name=viewport]')
     metaTag.attr('content','initial-scale=1, maximum-scale=1, minimum-scale=1')
     console.log(metaTag);
+    gameDOM.bind('vclick',function(e){
+        gameDOM.css({'backgroundColor': 'green'});
+    });
 });
 
 
