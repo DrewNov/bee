172cec4d58f37bf8e1c18791f3946cf8add1c5d8
DrewNov <andrey7777777@yandex.ru>
2012-08-08 17:33:34 +0300

    2nd Day: movement added, but very slow on Tablet PC

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 $(document).bind("mobileinit", function(){
     $.mobile.hidePageLoadingMsg();
 });
-$(document).ready(function(){
 
+$(document).ready(function(){
+    bug = $('.bug');
     var gameDOM  = $('.game');
-    gameDOM.css({'height': window.screen.availHeight-15+'px', 'width': window.height+'px'});
+    gameDOM.css({'height': window.screen.availHeight-15+'px', 'width': window.screen.width+'px'});
 
     var metaTag = $('meta[name=viewport]')
     metaTag.attr('content','initial-scale=1, maximum-scale=1, minimum-scale=1')
+
     console.log(metaTag);
-    gameDOM.bind('vclick',function(e){
-        gameDOM.css({'backgroundColor': 'green'});
+
+    var id_interval = 0;
+
+    gameDOM.bind('vmousedown',function(e){
+        console.log('---START');
+        clearInterval(id_interval);
+        id_interval = setInterval(function(){moveBug(e)}, 0);
+        //bug.animate({left: '+=10'}, 100, 'linear');
+    });
+
+    gameDOM.bind('vmouseup',function(){
+        clearInterval(id_interval);
+        //bug.stop();
+        console.log('---STOP');
     });
 });
 
