fb2623aa64c3dc854760975700b4cf728dba85dd
Voltash <Volt.zt@gmail.com>
2012-08-08 13:05:44 +0300

    fix bug with px

--- js/main.js
+++ js/main.js
@@ -5,13 +5,14 @@ $(document).ready(function(){
 
     var gameDOM  = $('.game');
     gameDOM.css({'height': window.screen.availHeight-15+'px', 'width': window.height+'px'});
-
     var metaTag = $('meta[name=viewport]')
     metaTag.attr('content','initial-scale=1, maximum-scale=1, minimum-scale=1')
-    console.log(metaTag);
-    gameDOM.bind('vclick',function(e){
-        gameDOM.css({'backgroundColor': 'green'});
-    });
+
+    var objectBug = new bug({idGameBlock:'gameBug'});
+
+
+
+
 });
 
 
