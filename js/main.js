2c15d3e88e39a5b3538fac04ac6b57c854156b35
DrewNov <andrey7777777@yandex.ru>
2012-08-08 17:54:26 +0300

    2nd Day: small fixes

--- js/main.js
+++ js/main.js
@@ -3,26 +3,23 @@ $(document).bind("mobileinit", function(){
 });
 
 $(document).ready(function(){
-    bug = $('.bug');
     var gameDOM  = $('.game');
     gameDOM.css({'height': window.screen.availHeight-15+'px', 'width': window.height+'px'});
     var metaTag = $('meta[name=viewport]')
     metaTag.attr('content','initial-scale=1, maximum-scale=1, minimum-scale=1')
 
     var objectBug = new bug({idGameBlock:'gameBug'});
-
+    bug = $('.bug');
     var id_interval = 0;
 
     gameDOM.bind('vmousedown',function(e){
         console.log('---START');
         clearInterval(id_interval);
         id_interval = setInterval(function(){moveBug(e)}, 0);
-        //bug.animate({left: '+=10'}, 100, 'linear');
     });
 
     gameDOM.bind('vmouseup',function(){
         clearInterval(id_interval);
-        //bug.stop();
         console.log('---STOP');
     });
 });
