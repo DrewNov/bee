12b0ebf29e88d935a244df765c0c6150a4243105
DrewNov <andrey7777777@yandex.ru>
2012-08-09 16:00:33 +0300

    3nd Day: added flipping of the bug when move

--- js/navigation.js
+++ js/navigation.js
@@ -11,13 +11,31 @@ var moveBug = function (e) {
             return false;
         }
         bugX -= step;
+        flipBug(-1);
     } else {
         console.log('Right');
-        if (bugX >= halfWidth*2-45) {
+        if (bugX >= halfWidth * 2 - 45) {
             return false;
         }
         bugX += step;
+        flipBug(1);
     }
 
     bug.css("left", bugX + "px");
+}
+
+var flipBug = function (e) {
+    var bug = $('.bug');
+    var scaleCof = $('#gameBug').height()/514  ;
+    var imgBugHeight = 49,
+        imgBugWidth = 61,
+        bottom = 29;
+
+    if (e == 1) {
+        bug.css("background", "url('../img/bug_inv.png') no-repeat");
+    } else {
+        bug.css("background", "url('../img/bug.png') no-repeat");
+    }
+
+    bug.css({'width':imgBugWidth*scaleCof+'px', 'height':imgBugHeight*scaleCof+'px', 'backgroundSize':'cover'});
 }
