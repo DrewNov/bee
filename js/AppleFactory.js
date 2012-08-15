006acea899f76560763fb7821d1930f68be8bfb0
Voltash <Volt.zt@gmail.com>
2012-08-15 17:03:20 +0300

    add apple count

--- js/AppleFactory.js
+++ js/AppleFactory.js
@@ -13,7 +13,7 @@ $(document).ready(
 
     function(){
         // Initialize CSSClass Array
-        for (var i=0; i<10; i++)
+        for (var i=0; i<30; i++)
         {
             globalGeneratedApple.push(new Apple(i));
 
